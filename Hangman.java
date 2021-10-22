88c2db922d60bfc8cad1255a8d297199a45555bf
mariuskern <mariusskern@gmail.com>
2021-10-22 15:49:48 +0200

    Autosave

--- Hangman.java
+++ Hangman.java
@@ -8,6 +8,13 @@ public class Hangman {
 
     public void refresh() throws Exception{
 
-      new ProcessBuilder("cmd","/c", "cls").inheritIO().start().waitFor();
+        if(Control.getOS().equals("Windows 10")) {
+            new ProcessBuilder("cmd","/c", "cls").inheritIO().start().waitFor();
+        } else {
+            new ProcessBuilder("clear").inheritIO().start().waitFor();
+        }
+
+        
+
     }
 }
--- main.java
+++ main.java
@@ -2,7 +2,9 @@ class MAIN{
 
   public static void main(String[] args) throws Exception{
 
-    System.out.println(Control.getOS());
+    Hangman a = new Hangman();
+
+    a.refresh();
 
   }
 }
